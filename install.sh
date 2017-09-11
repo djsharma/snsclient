bea659eeb8712edb778205c860237621863a6a57
Dhananjay Sharma <dhananjaysharma@Dhananjays-MacBook-Air.local>
2017-09-11 11:28:18 +0530

    [add install.sh]

---
+++ install.sh
@@ -0,0 +1,9 @@
+# @Author: dhananjaysharma
+# @Date:   2017-09-11 11:26:26
+# @Last Modified by:   dhananjaysharma
+# @Last Modified time: 2017-09-11 11:27:57
+sudo apt-get update
+sudo apt-get install python-pip
+sudo pip install virtualenv
+cd ..
+virtualenv env
