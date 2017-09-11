d9077063cc495c5c213fb85e1c2cd8492ca1b85a
Dhananjay Sharma <dhananjaysharma@Dhananjays-MacBook-Air.local>
2017-09-11 11:51:13 +0530

    [update install.sh]

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 # @Author: dhananjaysharma
 # @Date:   2017-09-11 11:26:26
 # @Last Modified by:   dhananjaysharma
-# @Last Modified time: 2017-09-11 11:27:57
+# @Last Modified time: 2017-09-11 11:31:03
 sudo apt-get update
 sudo apt-get install python-pip
 sudo pip install virtualenv
 cd ..
 virtualenv env
+source env/bin/activate
+pip install requests
+pip install boto
+pip install django
